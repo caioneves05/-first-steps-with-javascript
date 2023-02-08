3b1d4e1f674bd8ba6d3b1ea615244c2284172175
caioneves05 <ncaio037@gmail.com>
2023-02-08 16:21:53 -0300

    Começando os estudos sobre Promises e programação asíncrona.

--- Alura/criandoUmaLibrary/index.js
+++ Alura/criandoUmaLibrary/index.js
@@ -1,4 +1,33 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
 
-console.log(chalk.green("Olá mundo"));
+function trataErro(error) {
+    // Instanciado um novo Objeto error
+    throw new Error(error);
+}
+/* 
+function pegaArquivo(data) {
+    const encoding = 'utf8';
+    fs.readFile(data, encoding, (erro, texto) =>{
+        if(erro){
+            trataErro(erro,"Não há arquivo no diretório.")
+        }
+        console.log(chalk.green(texto));
+    });
+    
+};
+*/
+function pegaArquivo(data) {
+
+    const enconding = 'utf-8';
+    //promise
+    fs.promises
+        .readFile(data, enconding)
+        //resolve a promise e retorna o valor dela.
+        .then((text)=> console.log(chalk.blue(text)))
+        //em caso der erro, ele retornará error.
+        .catch((trataErro))
+}
+
+pegaArquivo('./arquivos/texto.md');
