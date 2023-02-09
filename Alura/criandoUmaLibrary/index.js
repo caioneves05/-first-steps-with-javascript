dcfda874eb1765efe9493b122b0e90dc2a433adf
caioneves05 <ncaio037@gmail.com>
2023-02-09 16:32:15 -0300

    .

--- Alura/criandoUmaLibrary/index.js
+++ Alura/criandoUmaLibrary/index.js
@@ -2,32 +2,37 @@ import chalk from 'chalk';
 import fs from 'fs';
 
 
+const textoTest = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.). [Teste de retorno 400](https://httpstat.us/404). [gatinho salsicha](http://gatinhosalsicha.com.br/)";
+
+function extraiLinks(text) {
+    // as espressões regulares tem que ser colocadas dentr de /espressão/
+    const regexp = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm ;
+    const capturas = [...text.matchAll(regexp)];
+    
+    console.log(chalk.blue(capturas))
+}
+
+
 function trataErro(error) {
     // Instanciado um novo Objeto error
-    throw new Error(error);
+    throw new Error(chalk.red(error.code, ': Não há nenhum arquivo.'));
 }
-/* 
-function pegaArquivo(data) {
-    const encoding = 'utf8';
-    fs.readFile(data, encoding, (erro, texto) =>{
-        if(erro){
-            trataErro(erro,"Não há arquivo no diretório.")
-        }
-        console.log(chalk.green(texto));
-    });
+
+
+
+async function pegaArquivo(data) {
+    try {
+        const encoding ='utf-8'
     
-};
-*/
-function pegaArquivo(data) {
-
-    const enconding = 'utf-8';
-    //promise
-    fs.promises
-        .readFile(data, enconding)
-        //resolve a promise e retorna o valor dela.
-        .then((text)=> console.log(chalk.blue(text)))
-        //em caso der erro, ele retornará error.
-        .catch((trataErro))
+    const text = await fs.promises.readFile(data, encoding);
+    console.log(chalk.green(text));
+    } catch(error){
+        trataErro(error)
+    } finally {
+        console.log(chalk.yellow("Operação finalizada."))
+    }
 }
 
-pegaArquivo('./arquivos/texto.md');
+
+
+extraiLinks(textoTest);
